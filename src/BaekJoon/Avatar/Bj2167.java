cc6a005082d13db1eb6c58d964c6fc5534439a9c
Lee JunBeon <skier5000@naver.com>
2021-07-30 15:35:55 +0900

    Bj 2167 2차원 배열의 합 추가

---
+++ src/BaekJoon/Avatar/Bj2167.java
@@ -0,0 +1,46 @@
+package BaekJoon.Avatar;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+/*
+2 3
+1 2 4
+8 16 32
+3
+1 1 2 3
+1 2 1 2
+1 3 2 3
+ */
+public class Bj2167 {
+    public static void main(String args[]) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int dp[][] = new int[N + 1][M + 1];
+
+        for (int i = 1; i <= N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 1; j <= M; j++) {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1] - dp[i - 1][j - 1] + Integer.parseInt(st.nextToken());
+            }
+        }
+
+        int T = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < T; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x1 = Integer.parseInt(st.nextToken());
+            int y1 = Integer.parseInt(st.nextToken());
+            int x2 = Integer.parseInt(st.nextToken());
+            int y2 = Integer.parseInt(st.nextToken());
+            bw.write(String.valueOf(dp[x2][y2] + dp[x1 - 1][y1 - 1] - dp[x1 - 1][y2] - dp[x2][y1 - 1]));
+            bw.write("\n");
+        }
+
+        bw.flush();
+    }
+}
