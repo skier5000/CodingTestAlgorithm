dacd7068706f658dad6eb40d7840e83531d8c00e
Junbeom Lee <skier5000@naver.com>
2021-10-13 20:38:07 +0900

    브루트포스 블랙잭 문제 생성

---
+++ src/BaekJoon/단계별로풀어보기/브루트포스/블랙잭.java
@@ -0,0 +1,13 @@
+package BaekJoon.단계별로풀어보기.브루트포스;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 블랙잭 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+    }
+}
