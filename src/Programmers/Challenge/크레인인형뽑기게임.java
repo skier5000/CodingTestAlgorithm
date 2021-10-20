e0e4ef69dc5dbf8b8cc4b60985e1268be3c62b8c
Lee JunBeon <skier5000@naver.com>
2021-10-20 15:22:30 +0900

    크레인 인형뽑기 게임 - 반례 탐색 요망

--- src/Programmers/Challenge/크레인인형뽑기게임.java
+++ src/Programmers/Challenge/크레인인형뽑기게임.java
@@ -6,6 +6,11 @@ import java.util.Queue;
 import java.util.Stack;
 
 public class 크레인인형뽑기게임 {
+
+    /**
+     * 반례
+     */
+
     public static void main(String[] args) {
         int[][] board = {{0, 0, 0, 0, 0}, {0, 0, 1, 0, 3}, {0, 2, 5, 0, 1}, {4, 2, 4, 4, 2}, {3, 5, 1, 3, 1}};
         int[] moves = {1, 5, 3, 5, 1, 2, 1, 4};
