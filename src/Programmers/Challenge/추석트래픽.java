c72427bc7150c3297d3d86a17475be8602559355
Lee JunBeon <skier5000@naver.com>
2021-10-21 20:46:29 +0900

    추석 트래픽 문제 생성

---
+++ src/Programmers/Challenge/추석트래픽.java
@@ -0,0 +1,34 @@
+package Programmers.Challenge;
+
+public class 추석트래픽 {
+    public static void main(String[] args) {
+        추석트래픽 dd = new 추석트래픽();
+
+        String[] question1 = {"2016-09-15 01:00:04.001 2.0s", "2016-09-15 01:00:07.000 2s"}; // 1
+        String[] question2 = {"2016-09-15 01:00:04.002 2.0s", "2016-09-15 01:00:07.000 2s"}; // 2
+        String[] question3 = {
+                "2016-09-15 20:59:57.421 0.351s",
+                "2016-09-15 20:59:58.233 1.181s",
+                "2016-09-15 20:59:58.299 0.8s",
+                "2016-09-15 20:59:58.688 1.041s",
+                "2016-09-15 20:59:59.591 1.412s",
+                "2016-09-15 21:00:00.464 1.466s",
+                "2016-09-15 21:00:00.741 1.581s",
+                "2016-09-15 21:00:00.748 2.31s",
+                "2016-09-15 21:00:00.966 0.381s",
+                "2016-09-15 21:00:02.066 2.62s"}; // 7
+
+
+        dd.solution(question1);
+        dd.solution(question2);
+        dd.solution(question3);
+    }
+
+    public int solution(String[] lines) {
+        int answer = 0;
+
+
+
+        return answer;
+    }
+}
