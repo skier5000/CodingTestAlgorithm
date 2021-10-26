6cda465eeb3a96571f16b0e3ac6c10b8c148484c
Junbeom Lee <skier5000@naver.com>
2021-10-26 19:38:44 +0900

    124나라의 숫자

---
+++ src/Programmers/Challenge/CountryOf124.java
@@ -0,0 +1,28 @@
+package Programmers.Challenge;
+
+public class CountryOf124 {
+    public static void main(String[] args) {
+        CountryOf124 dd = new CountryOf124();
+
+
+
+        System.out.println(dd.solution(1)); // 1
+        System.out.println(dd.solution(2)); // 2
+        System.out.println(dd.solution(3)); // 4
+        System.out.println(dd.solution(4)); // 11
+    }
+
+    public String solution(int n) {
+        String answer = "";
+
+        int[] countryOf124Number = {1, 2, 4};
+
+        for (int i = 0; i < n; i++) {
+
+        }
+
+
+
+        return answer;
+    }
+}
