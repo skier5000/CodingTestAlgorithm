a7938114b26f8df9b8570106e77f6dec1de84570
Lee JunBeon <skier5000@naver.com>
2021-10-31 14:17:12 +0900

    124 나라의 숫자

--- src/Programmers/Challenge/CountryOf124.java
+++ src/Programmers/Challenge/CountryOf124.java
@@ -6,22 +6,33 @@ public class CountryOf124 {
 
 
 
-        System.out.println(dd.solution(1)); // 1
-        System.out.println(dd.solution(2)); // 2
-        System.out.println(dd.solution(3)); // 4
-        System.out.println(dd.solution(4)); // 11
+//        System.out.println(dd.solution(1)); // 1
+//        System.out.println(dd.solution(2)); // 2
+//        System.out.println(dd.solution(3)); // 4
+//        System.out.println(dd.solution(4)); // 11
+
+
+        System.out.println(dd.solution(10));
     }
 
     public String solution(int n) {
+        String[] numbers = {"4", "1", "2"};
         String answer = "";
 
-        int[] countryOf124Number = {1, 2, 4};
+        int num = n;
 
-        for (int i = 0; i < n; i++) {
+        // input 만큼 돌리기
+        while(num > 0){
+            // remainder 에 num/3 의 나머지
+            // num 은 몫
+            int remainder = num % 3;
+            num = num / 3;
 
+            if(remainder == 0)
+                num--;
+
+            answer = numbers[remainder] + answer;
         }
-
-
 
         return answer;
     }
